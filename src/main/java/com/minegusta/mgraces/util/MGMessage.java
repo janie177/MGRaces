25e1e87097e51eb728d020080f6bb7d58c88ed33
Jan <janie177@hotmail.com>
2014-06-27 17:48:47 +0200

    Fixed chat color in prefix for MGMessage.

--- src/main/java/com/minegusta/mgraces/util/MGMessage.java
+++ src/main/java/com/minegusta/mgraces/util/MGMessage.java
@@ -7,7 +7,7 @@ import org.bukkit.entity.Player;
 public class MGMessage
 {
     private static DefaultConf conf = new DefaultConf();
-    private static String prefix = conf.getPrefix();
+    private static String prefix = ChatColor.translateAlternateColorCodes('&', conf.getPrefix());
 
     public static void message(Player p, String text)
     {

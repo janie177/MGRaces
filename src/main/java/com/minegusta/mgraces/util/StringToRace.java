9106d1414a3b600cdd00400fb7a13af3bc19802b
Jan <janie177@hotmail.com>
2014-11-23 04:39:36 +0100

    Fixed a small error.

--- src/main/java/com/minegusta/mgraces/util/StringToRace.java
+++ src/main/java/com/minegusta/mgraces/util/StringToRace.java
@@ -8,7 +8,7 @@ public enum StringToRace
     human(new Human(), ChatColor.YELLOW + "[Human]"),
     elf(new Elf(), ChatColor.GREEN + "[Elf]"),
     enderborn(new EnderBorn(), ChatColor.DARK_PURPLE + "[EnderBorn]"),
-    demon(new Demon(), ChatColor.RED + "{Demon]"),
+    demon(new Demon(), ChatColor.RED + "[Demon]"),
     aurora(new Aurora(), ChatColor.AQUA + "[Aurora]"),
     dwarf(new Dwarf(), ChatColor.DARK_GRAY + "[Dwarf]");
 
